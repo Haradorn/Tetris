87a050410346a60794aec86a97b346d92ff7810b
Haradorn <shewingtrue@gmail.com>
2020-02-04 19:41:10 +0300

    Tetris main functional
    Old sweet game.

---
+++ TETRIS/TETRIS/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TETRIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new TETRIS.Form1.MyPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(246, 379);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(287, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(297, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 68);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Indigo;
+            this.groupBox1.Location = new System.Drawing.Point(287, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 118);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Скорость";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 73);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(100, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Быстрая";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 33);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(124, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Медленная";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Indigo;
+            this.checkBox1.Location = new System.Drawing.Point(287, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Высокий";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(461, 411);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TETRIS by Haradorn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MyPanel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+
+        public class MyPanel : System.Windows.Forms.Panel
+        {
+            public void enableDB()
+            {
+                DoubleBuffered = true;
+            }
+        }
+    }
+}
+
---
+++ TETRIS/TETRIS/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace TETRIS
+{
+    public partial class Form1 : Form
+    {
+        int MAXX = 10, MAXY = 15, ps = 24;
+        bool started = false;
+        Brush[] brsh = {Brushes.White, Brushes.Orange, Brushes.Blue, Brushes.Red, Brushes.Green, Brushes.Honeydew, Brushes.Violet,
+                       Brushes.Tomato, Brushes.SteelBlue, Brushes.PapayaWhip};
+        int score;
+        int[,] arr;
+        Figure figure;
+        Graphics grph;
+        SoundPlayer simplesound = new SoundPlayer("chimes.wav"),
+        simplesoundEnd = new SoundPlayer("fart.wav"),
+        simplesoundStart = new SoundPlayer("start.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void upd_score()
+        {
+            label1.Text = score.ToString();
+        }
+        private void draw(int x, int y, int clr)
+        {
+            if (clr == 0 || x < 0 || x >= MAXX || y >= MAXY) return;
+            grph.FillEllipse(brsh[clr], new Rectangle(2 + x * ps, 2 + y * ps, ps, ps));
+        }
+        private void draw_field()
+        {
+            int i, j;
+            for (i = 0; i < MAXX; ++i)
+                for (j = 0; j < MAXY; ++j)
+                    draw(i, j, arr[i, j]);
+        }
+        private void draw_figure(Figure f)
+        {
+            int i, j;
+            for (i = 0; i < 4; ++i)
+                for (j = 0; j < 4; ++j)
+                    draw(f.x + i, f.y + j, f.pix[i, j]);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(figure == null)
+            {
+                figure = new Figure();
+                if(!figure_ok())
+                {
+                    timer1.Enabled = false;
+                    simplesoundEnd.Play();
+                    MessageBox.Show("Game Over!");
+                }
+            }
+            figure_down();
+            panel1.Refresh();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            started = true;
+            score = 0;
+            if (checkBox1.Checked == true)
+            {
+                MAXY = 25;
+                panel1.Height *= MAXY;
+                this.Height = 700;
+            }
+            else
+            {
+                MAXY = 15;
+                this.Height = 470;
+            }
+            arr = new int[MAXX, MAXY];
+            upd_score();
+            figure = null;
+            timer1.Enabled = true;
+            if (radioButton1.Checked == true) timer1.Interval = 400;
+            if (radioButton2.Checked == true) timer1.Interval = 100;
+            simplesoundStart.Play();
+        }
+        private int get_arr(int x, int y)
+        {
+            if (x < 0 || x >= MAXX || y >= MAXY) return 1;
+            if (y < 0) return 0;
+            return arr[x, y];
+        }
+        private void set_arr(int x, int y, int clr)
+        {
+            if (x < 0 || x >= MAXX || y < 0 || y >= MAXY) return;
+            arr[x, y] = clr;
+        }
+        private bool figure_ok()
+        {
+            int i, j;
+            for(i = 0; i < 4; ++i)
+                for(j = 0; j < 4; ++j)
+                {
+                    if (figure.pix[i, j] != 0 && get_arr(figure.x + i, figure.y + j) != 0)
+                        return false;
+                }
+            return true;
+        }
+        private void apply_figure()
+        {
+            int i, j;
+            for (i = 0; i < 4; ++i)
+                for (j = 0; j < 4; ++j)
+                    if (figure.pix[i, j] != 0) set_arr(figure.x + i, figure.y + j, figure.pix[i, j]);
+            bool[] lines = new bool[MAXY];
+            int goo = 0;
+            for(i = 0; i < MAXY; ++i)
+            {
+                bool qq = true;
+                for(j = 0; j < MAXX; ++j)
+                {
+                    if(arr[j, i] == 0)
+                    {
+                        qq = false;
+                        break;
+                    }
+                }
+                lines[i] = qq;
+                if (qq) ++goo;
+            }
+            if(goo != 0)
+            {
+                int sc = (goo * (goo + 1) / 2) * 100;
+                score += sc;
+                upd_score();
+                simplesound.Play();
+            }
+            int[,] nar = new int[MAXX, MAXY];
+            int ty = MAXY;
+            for(i = MAXY - 1; i >= 0; --i)
+            {
+                if(lines[i]) continue;
+                --ty;
+                for(j = 0; j < MAXX; ++j)
+                {
+                    nar[j, ty] = arr[j, i];
+                }
+            }
+            arr = nar;
+        }
+        private void figure_down()
+        {
+            ++figure.y;
+            if(!figure_ok())
+            {
+                --figure.y;
+                apply_figure();
+                figure = null;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!started) return;
+            grph = e.Graphics;
+            grph.DrawRectangle(Pens.LightBlue, new Rectangle(0, 0, ps * MAXX + 4, ps * MAXY + 4));
+            grph.FillRectangle(Brushes.DodgerBlue, new Rectangle(1, 1, ps * MAXX + 3, ps * MAXY + 3));
+            draw_field();
+            if (figure != null) draw_figure(figure);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (figure == null) return;
+            if (e.KeyCode == Keys.A)
+            {
+                figure.x -= 1;
+                if (!figure_ok()) figure.x += 1;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                figure.x += 1;
+                if (!figure_ok()) figure.x -= 1;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                figure.y += 1;
+                if (!figure_ok()) figure.y -= 1;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                figure.rotate();
+                if(!figure_ok())
+                {
+                    for (int i = 0; i < 3; ++i) figure.rotate();
+                }
+            }
+            panel1.Refresh();
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Form1_KeyDown(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.enableDB();
+        }
+    }
+        public class Figure
+        {
+            string[] str = {"....1111........", "....111.1.......", ".....11..11.....", ".1...11...1.....",
+                           "....111..1......", "....111...1.....", "..1..11..1......"};
+            public int x, y;
+            public int clr;
+            public int[,] pix;
+            public Random rnd;
+            public Figure()
+            {
+                x = 3; y -= 2;
+                rnd = new Random();
+                clr = 1 + rnd.Next(9);
+                int n = rnd.Next(str.Length);
+                fillpix(out pix, str[n]);
+                n = rnd.Next(4);
+                for(int i = 0; i < n; ++i) 
+                    rotate();
+            }
+                void fillpix(out int[,] pix, string cc)
+                {
+                    pix = new int[4, 4];
+                    int i, j;
+                    for (i = 0; i < 4; ++i)
+                        for (j = 0; j < 4; ++j)
+                        {
+                            if (cc[j * 4 + i] == '1') pix[i, j] = clr;
+                            else pix[i, j] = 0;
+                        }       
+                }
+                public void rotate()
+                {
+                    int[,] p2 = new int[4, 4];
+                    int i, j;
+                    for(i = 0; i < 4; ++i)
+                        for(j = 0; j < 4; ++j)
+                        {
+                            p2[3 - j, i] = pix[i, j];
+                        }
+                    pix = p2;    
+                }
+        }
+}
